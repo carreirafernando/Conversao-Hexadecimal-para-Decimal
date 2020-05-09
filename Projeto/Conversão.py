eb3cd3e772a1e9a9d82ad39e9e185200a71ae456
Carreira Fernando <fe.alves_carreira@hotmail.com>
2020-05-09 15:31:36 -0300

    Criação de código

---
+++ Projeto/Conversão.py
@@ -0,0 +1,31 @@
+def verificacao():
+    if i == 'a':
+        return 10
+    elif i == 'b':
+        return 11
+    elif i == 'c':
+        return 12
+    elif i == 'd':
+        return 13
+    elif i == 'e':
+        return 14
+    elif i == 'f':
+        return 15
+
+
+hex = input('Digite: ')
+lista = []
+for i in hex:
+    if i in '0123456789':
+        lista.append(int(i))
+    else:
+        num = verificacao()
+        lista.append(num)
+
+print(lista)
+total = 0
+potencia = len(lista)
+for i in lista:
+    potencia -= 1
+    total += i * (16 ** potencia)
+print(total)
